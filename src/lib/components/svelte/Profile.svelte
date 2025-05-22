fa21a0579681f2aab05965eda83f8f4a8c3abe2a
grape <grapeberryfruit@gmail.com>
2025-05-22 05:46:03 -0600

    pretty front page

---
+++ src/lib/components/svelte/Profile.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu/index.js';
+	import { User } from 'lucide-svelte';
+	import Button from '../ui/button/button.svelte';
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger>
+		<Button size="icon" variant="outline">
+			<User class="h-6 w-6" />
+		</Button>
+	</DropdownMenu.Trigger>
+</DropdownMenu.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
-	import Nav from '$lib/components/svelte/Nav.svelte';
+	import NavBar from '$lib/components/svelte/NavBar.svelte';
+	import { ModeWatcher } from 'mode-watcher';
 
 	import '../app.css';
 
 	import { Separator } from '$lib/components/ui/separator/index.js';
+	import Profile from '$lib/components/svelte/Profile.svelte';
+	import DarkMode from '$lib/components/svelte/darkMode.svelte';
 
 	let { children } = $props();
 </script>
 
+<ModeWatcher />
+
 <main class="h-auto w-full">
-	<Nav />
+	<div class="my-2 flex">
+		<NavBar />
+		<div class="mx-5 my-1 flex gap-5">
+			<DarkMode />
+			<Profile />
+		</div>
+	</div>
 	<Separator />
-	<div class="m-4 rounded-lg bg-white p-2 text-black">
+	<div class="m-4 rounded-lg">
 		{@render children?.()}
 	</div>
 </main>
